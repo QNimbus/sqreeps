60674360bb2bfc4dc49adfdbcf95e8828ae314c6
B. van Wetten <bas.van.wetten@gmail.com>
2019-02-22 19:55:22 +0100

    Stash

--- src/memory/MemorySegmenter.ts
+++ src/memory/MemorySegmenter.ts
@@ -19,12 +19,50 @@ interface IMemorySegmenter {
 	publicSegments: Array<number>;
 }
 
-const MemorySegmenterDefaults = {};
+const MAX_ACTIVE_SEGMENTS = 10;
+
+const MemorySegmenterDefaults: IMemorySegmenter = {
+	activeSegments: [],
+	activeForeignSegment: undefined,
+	publicSegments: [],
+};
 
 export class MemorySegmenter {
-	private static cache: IMemorySegmenterCache = {
+	private static _instance: MemorySegmenter;
+
+	private segmenter: IMemorySegmenter;
+
+	private _cache: IMemorySegmenterCache = {
 		segments: {},
 		lastAccessed: {},
 		lastModified: {},
 	};
+
+	private constructor() {
+		this.segmenter = _.defaultsDeep({}, MemorySegmenterDefaults);
+	}
+
+	public static get Instance(): MemorySegmenter {
+		return this._instance || (this._instance = new this());
+	}
+
+	public static get memory(): IMemorySegmenter {
+		return this._instance.segmenter;
+	}
+
+	public static get cache(): IMemorySegmenterCache {
+		return this._instance._cache;
+	}
+
+	public static requestSegments(...ids: Array<number>) {
+		for (let id of ids) {
+			if (!this.memory.activeSegments.includes(id)) {
+				this.memory.activeSegments.push(id);
+				if (this.memory.activeSegments.length > MAX_ACTIVE_SEGMENTS) {
+					let removeSegment = this.memory.activeSegments.shift();
+					console.log(`Maximum number of active memory segments reached; removing segment ${removeSegment}`);
+				}
+			}
+		}
+	}
 }
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -2,6 +2,7 @@
 
 import { log } from './console/log';
 import { Qreep } from 'qreep/Qreep';
+import { MemorySegmenter } from 'Memory/MemorySegmenter';
 
 let json = (obj: {}) => {
 	JSON.stringify(obj, undefined, '\t');
@@ -10,27 +11,37 @@ let json = (obj: {}) => {
 export function sandbox() {
 	try {
 		let currentTick = Game.time;
-		console.log(`[${currentTick}] Segments count : ${Object.keys(RawMemory.segments).length}`);
-		console.log(`[${currentTick}] Segments count : ${JSON.stringify(Object.keys(RawMemory.segments))}`);
-		Object.keys(RawMemory.segments).forEach((key, index, array) => {
-			let sKey = parseInt(key);
-			console.log(`[${currentTick}] Segment[${key}] : ${RawMemory.segments[sKey]}`);
-		});
-		if (currentTick % 10 === 0) {
-			console.log(`[${currentTick}] Setting active segments to '[66]' for next tick`);
-			RawMemory.setActiveSegments([66]);
+		let ff = MemorySegmenter.Instance;
+		if (currentTick % 1 === 0) {
+			if (MemorySegmenter.memory.activeSegments.length === 0) {
+				log.debug(`Resetting memory....`);
+				MemorySegmenter.requestSegments(1, 2, 3, 4, 5, 6, 7, 8, 9);
+				MemorySegmenter.requestSegments(10, 11);
+			}
 		}
-		if (currentTick % 10 === 1) {
-			RawMemory.segments[66] = JSON.stringify({ testData: 'My Test' });
-		}
-		if (currentTick % 10 === 4) {
-			console.log(`[${currentTick}] Setting active segments to '[3]' for next tick`);
-			RawMemory.setActiveSegments([3]);
-		}
-		if (currentTick % 10 === 7) {
-			console.log(`[${currentTick}] Setting active segments to '[3, 66]' for next tick`);
-			RawMemory.setActiveSegments([3, 66]);
-		}
+		// log.debug(JSON.stringify(ff));
+
+		// console.log(`[${currentTick}] Segments count : ${Object.keys(RawMemory.segments).length}`);
+		// console.log(`[${currentTick}] Segments count : ${JSON.stringify(Object.keys(RawMemory.segments))}`);
+		// Object.keys(RawMemory.segments).forEach((key, index, array) => {
+		// 	let sKey = parseInt(key);
+		// 	console.log(`[${currentTick}] Segment[${key}] : ${RawMemory.segments[sKey]}`);
+		// });
+		// if (currentTick % 10 === 0) {
+		// 	console.log(`[${currentTick}] Setting active segments to '[66]' for next tick`);
+		// 	RawMemory.setActiveSegments([66]);
+		// }
+		// if (currentTick % 10 === 1) {
+		// 	RawMemory.segments[66] = JSON.stringify({ testData: 'My Test' });
+		// }
+		// if (currentTick % 10 === 4) {
+		// 	console.log(`[${currentTick}] Setting active segments to '[3]' for next tick`);
+		// 	RawMemory.setActiveSegments([3]);
+		// }
+		// if (currentTick % 10 === 7) {
+		// 	console.log(`[${currentTick}] Setting active segments to '[3, 66]' for next tick`);
+		// 	RawMemory.setActiveSegments([3, 66]);
+		// }
 	} catch (e) {
 		log.error(e);
 	}
